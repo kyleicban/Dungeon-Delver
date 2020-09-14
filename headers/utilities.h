0a3bc37468bab626b1efcf0021b94becf37164f2
Kyle Icban <kyleicban@yahoo.com>
2020-09-13 22:08:22 -0700

    changed controls to support w,a,s,d movement

--- headers/utilities.h
+++ headers/utilities.h
@@ -1,19 +1,17 @@
 // utilities.h
 
-// Note:  YOU MUST NOT MAKE ANY CHANGE TO THIS FILE!
-
 #ifndef UTILITIES_INCLUDED
 #define UTILITIES_INCLUDED
 
-const char ARROW_LEFT = 'h';
-const char ARROW_RIGHT = 'l';
-const char ARROW_UP = 'k';
-const char ARROW_DOWN = 'j';
+const char ARROW_LEFT = 'a';
+const char ARROW_RIGHT = 'd';
+const char ARROW_UP = 'w';
+const char ARROW_DOWN = 's';
 
 char getCharacter();
 void clearScreen();
-int randInt(int lowest, int highest);  // random int from lowest to highest
-int randInt(int limit);                // random int from 0 to limit-1
-bool trueWithProbability(double p);    // return true with probability p
+int randInt(int lowest, int highest); // random int from lowest to highest
+int randInt(int limit);               // random int from 0 to limit-1
+bool trueWithProbability(double p);   // return true with probability p
 
 #endif // UTILITIES_INCLUDED
